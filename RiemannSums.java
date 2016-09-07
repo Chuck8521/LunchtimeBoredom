359337e369c8e458c1346de87352aea32a45712c
Chuck8521 <chuck8521@icloud.com>
2016-09-07 07:56:50 -0400

    Create RiemannSums.java

---
+++ RiemannSums.java
@@ -0,0 +1,14 @@
+//2008 - 3
+
+import java.util.*;
+
+public class RiemannSums {
+
+  public static void main(String[] args){
+  
+    Scanner input = new Scanner(System.in);
+    Scanner inputString = new Scanner(System.in);
+    int times = input.nextInt();
+    
+    for(int i = 0; i < times; i++){
+    
