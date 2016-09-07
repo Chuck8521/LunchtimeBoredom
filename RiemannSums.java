74090a17cd58a35b4323316ee22f7f228280fc7d
Chuck8521 <chuck8521@icloud.com>
2016-09-07 08:07:00 -0400

    Update RiemannSums.java

--- RiemannSums.java
+++ RiemannSums.java
@@ -11,4 +11,27 @@ public class RiemannSums {
     int times = input.nextInt();
     
     for(int i = 0; i < times; i++){
+      
+      String rawIn = inputString.nextLine();
+      String[] rawDoubs = rawIn.split(" ");
+      double a = Double.parseDouble(rawDoubs[0]);
+      double b = Double.parseDouble(rawDoubs[1]);
+      int rawM = Integer.parseInt(rawDoubs[2]);
+      int k = Integer.parseInt(rawDoubs[3]);
+      ArrayList<Integer> coefs = new ArrayList<Integer>();
+      
+      for(int x = 4; x <= k + 4; x++){
+        coefs.add(Double.parseDouble(rawDoubs[x]));
+      }
+      
+      //Maths and logics
+      double m = (double) rawM;
+      double rectWidth = (b - a)/m;
+      
+      //Pick up here
+      
+    }
     
+  }
+  
+}
