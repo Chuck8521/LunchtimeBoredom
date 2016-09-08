d8d6ad72dd42644b794267ebecb0db09d517df16
Chuck8521 <chuck8521@icloud.com>
2016-09-08 11:45:46 -0400

    Create LineSegmentRotation.java

---
+++ LineSegmentRotation.java
@@ -0,0 +1,20 @@
+//2008 - 5
+
+import java.util.*;
+
+public class LineSegmentRotation{
+
+  public static void main(String[] args){
+  
+    Scanner input = new Scanner(System.in);
+    int times = input.nextInt();
+    
+    for(int i = 0; i < times; i++){
+    
+      
+    
+    }
+  
+  }
+
+}
