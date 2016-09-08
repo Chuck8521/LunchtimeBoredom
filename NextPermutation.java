1466591d884aac0382f29f08e0358e6cc86924dc
Chuck8521 <chuck8521@icloud.com>
2016-09-07 21:49:56 -0400

    Working
    Input must be exact.

--- NextPermutation.java
+++ NextPermutation.java
@@ -1,4 +1,5 @@
 //2008 - 4
+//Input must be exact.
 
 import java.util.*;
 
@@ -12,7 +13,51 @@ public class NextPermutation {
     
     for(int i = 0; i < times; i++){
       
-      String rawIn = inputString.nextLine();
+      int numElements = input.nextInt();
+      String rawElements = inputString.nextLine();
+      String[] stringElements = rawElements.split(" ");
+      int[] elements = new int[stringElements.length];
+      ArrayList<Integer> processed = new ArrayList<Integer>();
+      
+      for(int x = 0; x < stringElements.length; x++){
+        elements[x] = Integer.parseInt(stringElements[x]);
+      }
+      
+      for(int x = numElements - 1; x > 0; x--){
+        
+        if(elements[x] > elements[x - 1]){
+          //We got the index
+          //Switch [x-1] with whatever the next largest value in processed is
+          int newValue = numElements;
+          processed.add(elements[x]);
+          for(int j = 0; j < processed.size(); j++){
+            if(processed.get(j) > elements[x - 1] && processed.get(j) < newValue){
+               newValue = processed.get(j);
+              processed.set(j, elements[x - 1]);
+            }
+          }
+          
+          //We have the right arraylist now, so output
+          Collections.sort(processed);//Is this right?
+          for(int j = 0; j < elements.length; j++){
+            System.out.print(elements[j] + " ");
+          }
+          System.out.println();
+          for(int j = 0; j < x - 1; j++){
+            System.out.print(elements[j] + " ");
+          }
+          System.out.print(newValue + " ");
+          for(int j = 0; j < processed.size(); j++){
+            System.out.print(processed.get(j) + " ");
+          }
+          System.out.println();
+          System.out.println();
+          break;
+        } else {
+          processed.add(elements[x]);
+        }
+        
+      }
       
     }
     
