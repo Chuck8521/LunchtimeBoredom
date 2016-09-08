57432057f78e31d975e9ec714b11e3e99f6868da
Chuck8521 <chuck8521@icloud.com>
2016-09-07 20:50:26 -0400

    Create NextPermutation.java

---
+++ NextPermutation.java
@@ -0,0 +1,21 @@
+//2008 - 4
+
+import java.util.*;
+
+public class NextPermutation {
+
+  public static void main(String[] args){
+  
+    Scanner input = new Scanner(System.in);
+    Scanner inputString = new Scanner(System.in);
+    int times = input.nextInt();
+    
+    for(int i = 0; i < times; i++){
+      
+      String rawIn = inputString.nextLine();
+      
+    }
+    
+  }
+  
+}
