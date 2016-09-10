bce1dda5821686c58bc8a40a75e9d23f4486e7f2
Chuck8521 <chuck8521@icloud.com>
2016-09-09 22:32:28 -0400

    Created and set basic format

---
+++ DirectedGraph.java
@@ -0,0 +1,24 @@
+//2008 - 5
+
+import java.util.*;
+
+public class DirectedGraph{
+
+  public static void main(String[] args){
+  
+    Scanner input = new Scanner(System.in);
+    Scanner inputS = new Scanner(System.in);
+    int times = input.nextInt();
+    
+    for(int i = 0; i < times; i++){
+    
+      String rawIn = inputS.nextLine();
+      String[] oneLine = rawIn.split(" ");
+      
+      
+      
+    }
+    
+  }
+  
+}
