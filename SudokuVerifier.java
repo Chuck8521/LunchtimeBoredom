3d2367df17a453aaecb0195cfa611511cbc21e35
Chuck8521 <chuck8521@icloud.com>
2016-09-12 07:45:30 -0400

    Basic Format

---
+++ SudokuVerifier.java
@@ -0,0 +1,21 @@
+//2007 - 5
+
+import java.util.*;
+
+public class SudokuVerifier{
+
+  public static void main(String[] args){
+  
+      Scanner input = new Scanner(System.in);
+      Scanner inputString = new Scanner(System.in);
+      int times = input.nextInt();
+    
+      for(int i = 0; i < times; i++){
+      
+        
+      
+      }
+      
+    }
+    
+}
