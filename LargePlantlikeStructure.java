02876e9f466ae397f6664d5dd56c7d783f38df99
Chuck8521 <chuck8521@icloud.com>
2016-09-14 08:10:09 -0400

    Update LargePlantlikeStructure.java

--- LargePlantlikeStructure.java
+++ LargePlantlikeStructure.java
@@ -9,8 +9,58 @@ public class LargePlantlikeStructure{
     Scanner input = new Scanner(System.in);
     Scanner inputS = new Scanner(System.in);
     int n = input.nextInt();
-    String[][] matrix= new String[n][n];
     
     for(int i = 0; i < n; i++){
-    
+      
+      int nodes = input.nextInt();
+      String raw = inputS.nextLine();
+      String[] rawNums = raw.split(" ");
+      int[] parentOf = new int[nodes];
+      for(int x = 0; x < nodes; x++){
+        parentOf[x] = Integer.parseInt(rawNums[x]);
+      }
+      
+      int pairs = input.nextInt();
+      for(int x = 0; x < pairs; x++){
+        String temp = inputS.nextLine();
+        String[] tempArray = temp.split(" ");
+        int rawStart = Integer.parseInt(tempArray[0]);
+        int rawEnd = Integer.parseInt(tempArray[1]);
+        
+        ArrayList<Integer> parentsOfStart = new ArrayList<Integer>(); 
+        ArrayList<Integer> parentsOfEnd = new ArrayList<Integer>(); 
+        
+        //Zero-based indexing
+        int start = rawStart - 1;
+        int end = rawEnd - 1;
+        
+        boolean go = true;
+        while(go){
+          parentsOfStart.add(parentOf[start]);
+          start = parentOf[start];
+          if(parentOf[start] == 0){
+            go = false;
+          }
+        }
+        
+        go = true;
+        while(go){
+          parentsOfEnd.add(parentOf[end]);
+          end = parentOf[end];
+          if(parentOf[end] == 0){
+            go = false;
+          }
+        }
+        
+        //Find first place where arrays converge TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
+        for(int j = 0; j < parentsOfStart.size(); j++){
+          for(int v = 0; v < parentsOfEnd.size(); v++){
+            if(parentsOfStart.get(j) == parentsOfEnd.get(v)){
+              //Switch array of focus
+            }
+          }
+        }
+        
+      }
+      
     }
