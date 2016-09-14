2530bb3f90f058ab3a570ce637f6801ea8f3a372
Chuck8521 <chuck8521@icloud.com>
2016-09-14 07:52:07 -0400

    Create LargePlantlikeStructure.java

---
+++ LargePlantlikeStructure.java
@@ -0,0 +1,16 @@
+//2006 - 6
+
+import java.util.*;
+
+public class LargePlantlikeStructure{
+
+  public static void main(String[] args){
+  
+    Scanner input = new Scanner(System.in);
+    Scanner inputS = new Scanner(System.in);
+    int n = input.nextInt();
+    String[][] matrix= new String[n][n];
+    
+    for(int i = 0; i < n; i++){
+    
+    }
