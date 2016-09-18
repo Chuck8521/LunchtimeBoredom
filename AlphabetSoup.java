8af3eab8585ef3d254502dc554faa898e0c143c6
Chuck8521 <chuck8521@icloud.com>
2016-09-17 22:09:57 -0400

    Create AlphabetSoup.java

---
+++ AlphabetSoup.java
@@ -0,0 +1,17 @@
+//2006 - #2
+
+import java.util.*;
+
+public class AlphabetSoup {
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    Scanner inputString = new Scanner(System.in);
+    int times = input.nextInt();
+
+    for(int i = 0; i < times; i++){
+    
+    }
+    
+}
