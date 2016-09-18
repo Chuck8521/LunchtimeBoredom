9e688e3f1c7b2c97f1d7ddcb08878d9eae48b271
Chuck8521 <chuck8521@icloud.com>
2016-09-17 21:51:48 -0400

    Not Working - Fix In Progress

--- XTremeSportsballPointThingy.java
+++ XTremeSportsballPointThingy.java
@@ -22,7 +22,7 @@ public class XTremeSportsballPointThingy{
          continue;
        }
        
-       int ways = 1;
+       ArrayList<String> allSets = new ArrayList<String>();
        //We have a positive integer. Do stuff with it
        int num3s = 0;
        for(int x = points; x > 0; x -= 3){
@@ -33,13 +33,43 @@ public class XTremeSportsballPointThingy{
              num1s++;
            }
            //I know here how many numbers of each are in this set - I need to find the number of unique permutations I can make
-           ways += (num1s + num2s + num3s) * (num1s + num2s + num3s);
+           ArrayList<Integer> set = new ArrayList<Integer>();
+           for(int a = 0; a < num1s; a++){
+             set.add(1);
+           }
+           for(int a = 0; a < num2s; a++){
+             set.add(2);
+           }
+           for(int a = 0; a < num3s; a++){
+             set.add(3);
+           }
+           
+           for(int a = 0; a < set.size(); a++){
+             int current = set.get(0);
+             for(int b = 0; b < set.size(); b++){
+               set.remove(b);
+               if(b == set.size()){
+                  set.add(current);
+               } else {
+                  set.add(b + 1, current);
+               }
+               //Convert to String because ArrayList deep copies are hard
+               String test = "";
+               for(int c = 0; c < set.size(); c++){
+                  test += set.get(c);
+               }
+               if(!allSets.contains(test)){
+                  allSets.add(test);
+               }
+             }
+           }
+           
            num2s++;
          }
          num3s++;
        }
       
-     System.out.println(points + " points: " + ways + " ways");
+     System.out.println(points + " points: " + allSets.size() + " ways");
       
     }
   }
