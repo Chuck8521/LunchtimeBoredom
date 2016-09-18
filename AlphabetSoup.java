d16674e18e25bc8b62d86cad8ebdcf9c48da8688
Chuck8521 <chuck8521@icloud.com>
2016-09-17 22:41:16 -0400

    Working

--- AlphabetSoup.java
+++ AlphabetSoup.java
@@ -11,7 +11,36 @@ public class AlphabetSoup {
     int times = input.nextInt();
 
     for(int i = 0; i < times; i++){
-    
+       String in = inputString.nextLine();
+             
+       int messages = input.nextInt();
+       for(int x = 0; x < messages; x++){
+         ArrayList<String> letters = new ArrayList<String>();
+         for(int c = 0; c < in.length(); c++){
+           letters.add(in.substring(c,c + 1).toLowerCase());
+         }
+         boolean valid = true;
+         String test = inputString.nextLine();
+         for(int j = 0; j < test.length(); j++){
+           if(!letters.contains(test.substring(j,j + 1).toLowerCase()) && Character.isLetter(test.charAt(j))){
+             valid = false;
+           } else if (Character.isLetter(test.charAt(j))) {
+             letters.remove(letters.indexOf(test.substring(j,j + 1).toLowerCase()));
+           }
+         }
+         
+         if(valid){
+           System.out.println(test + " : YES");
+         } else {
+           System.out.println(test + " : NO");
+         }
+         
+       }
+       
+       System.out.println();//As per requirments
+       inputString.nextLine();
     }
     
+}
+
 }
