3ba5fdca276c7b6974230206ab8a9907a80e5465
Chuck8521 <chuck8521@icloud.com>
2016-09-19 11:57:09 -0400

    Create EgyptFractToFract.java

---
+++ EgyptFractToFract.java
@@ -0,0 +1,19 @@
+//2007 - #2
+
+import java.util.*;
+
+public class EgyptFractToFract {
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    Scanner inputString = new Scanner(System.in);
+    int times = input.nextInt();
+
+    for(int i = 0; i < times; i++){
+    
+    }
+    
+  }
+  
+}
