20ae85114ab4b5f59d073f2352694d210073062f
Chuck8521 <chuck8521@icloud.com>
2016-09-19 11:45:48 -0400

    Create Speeding.java

---
+++ Speeding.java
@@ -0,0 +1,19 @@
+//1991 - #6
+
+import java.util.*;
+
+public class Speeding {
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    Scanner inputString = new Scanner(System.in);
+    int times = input.nextInt();
+
+    for(int i = 0; i < times; i++){
+    
+    }
+    
+  }
+  
+}
