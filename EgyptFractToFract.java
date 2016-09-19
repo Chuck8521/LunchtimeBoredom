b83fe4b6cfa0aec6d551ccef852eae4948945afa
Chuck8521 <chuck8521@icloud.com>
2016-09-19 12:15:35 -0400

    In Progress

--- EgyptFractToFract.java
+++ EgyptFractToFract.java
@@ -12,6 +12,36 @@ public class EgyptFractToFract {
 
     for(int i = 0; i < times; i++){
     
+      int numFractions = input.nextInt();
+      String rawDenoms = inputString.nextLine();
+      String[] raws = rawDenoms.split(" ");
+      int[] denoms = new int[raws.length];
+      int newDenom = 1;
+      int numer = 0;
+      for(int x = 0; x < raws.length; x++){
+        denoms[x] = Integer.parseInt(raws[x]);
+        newDenom *= denoms[x];
+      }
+      
+      for(int x = 0; x < denoms.length; x++){
+        numer += newDenom / denoms[x];;
+      }
+      
+      //Reduce numer/newDenom
+      for(int x = numer; x > 0; x--){
+        if(newDenom % x == 0){
+          newDenom /= x;
+          numer = x;
+        }
+      }
+      
+      for(int x = 0; x < denoms.length; x++){
+        if(x == denoms.length - 1){
+          //TODO
+        }
+        System.out.print("1/" + denoms[x] + " + ")
+      }
+      
     }
     
   }
