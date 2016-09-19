f9a9e286f76aeba37d3bbca9feafdb37176aea0a
Chuck8521 <chuck8521@icloud.com>
2016-09-19 11:54:59 -0400

    Ready to Test

--- Speeding.java
+++ Speeding.java
@@ -8,10 +8,27 @@ public class Speeding {
 
     Scanner input = new Scanner(System.in);
     Scanner inputString = new Scanner(System.in);
-    int times = input.nextInt();
-
-    for(int i = 0; i < times; i++){
-    
+    while(true){
+      System.out.println("Enter distance between checkpoints: ");
+      double distance = input.nextDouble();
+      if(distance == 0.0){
+        break;
+      }
+      System.out.println("Enter time between checkpoints: ");
+      double time = input.nextDouble();
+      System.out.println("Enter speed limit: ");
+      double speedlimit = input.nextDouble();
+      
+      speedLimit *= (5280.0/3600.0);
+      
+      if(speedLimit > (distance/time)){
+        System.out.println("SPEEDING");
+      } else {
+        System.out.println("NOT SPEEDING");
+      }
+      
+      System.out.println();
+      
     }
     
   }
