7f45ba6cacf6b0d7f5e186c3de599fc49faf972c
Chuck8521 <chuck8521@icloud.com>
2016-09-19 07:51:44 -0400

    Create SubstringSequence.java

---
+++ SubstringSequence.java
@@ -0,0 +1,19 @@
+//2007 - 6
+
+import java.util.*;
+
+public class SubstringSequence{
+
+  public static void main(String[] args){
+  
+    Scanner input = new Scanner(System.in);
+    Scanner inputString = new Scanner(System.in);
+    int times = input.nextInt();
+    
+    for(int i = 0; i < times; i++){
+    
+    }
+    
+  }
+  
+}
