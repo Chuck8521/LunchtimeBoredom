4002a9d41e73899f71fa8a0c06c8e7b4cf9f2695
Chuck8521 <chuck8521@icloud.com>
2016-09-19 08:02:43 -0400

    Ready to Test
    Might need to handle .split method exception

--- SubstringSequence.java
+++ SubstringSequence.java
@@ -1,4 +1,4 @@
-//2007 - 6
+//1998 - 3
 
 import java.util.*;
 
@@ -8,10 +8,44 @@ public class SubstringSequence{
   
     Scanner input = new Scanner(System.in);
     Scanner inputString = new Scanner(System.in);
-    int times = input.nextInt();
     
-    for(int i = 0; i < times; i++){
+    while(true){
     
+      System.out.print("Enter S: ");
+      String big = inputString.nextLine();
+      System.out.print("Enter T: ");
+      String small = inputString.nextLine();
+      if(big.equals("") && small.equals("")){
+        break;
+      }
+      
+      String[] letters = big.split("");
+      Arraylist<String> tests = small.split("");
+      int start = 81;
+      int end = 0;
+      boolean valid = true;
+      for(int i = 0; i < 0; i < letters){
+        String test = tests.get(0);
+        if(test.equals(letters[i])){
+          //Match
+          if(i < start){
+            start = i + 1;
+          }
+          tests.remove(0);
+          if(tests.size() == 0){
+            end = i + 1;
+            valid = false;
+            break;
+          }
+        }
+      }
+      
+      if(valid){
+        System.out.println(start + " " + end);
+      } else {
+        System.out.println("0");
+      }
+      
     }
     
   }
