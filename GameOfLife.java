9091072c14323b09a2c78e07e84fd038cd4f7067
Chuck8521 <chuck8521@icloud.com>
2016-09-20 21:32:41 -0400

    In Progress

--- GameOfLife.java
+++ GameOfLife.java
@@ -5,6 +5,9 @@ public class GameOfLife {
   public static void main(String[] args){
   
     //Begin!
+    List<String> lines = Files.readAllLines(Paths.get("path to file"), StandardCharsets.UTF_8);
+    
+    
   
   }
 
