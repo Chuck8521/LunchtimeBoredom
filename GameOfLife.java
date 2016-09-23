bf1be56a394b284be43a7a99a3d0359336fb0685
Chuck8521 <chuck8521@icloud.com>
2016-09-22 20:47:55 -0400

    Formatting and Working

--- GameOfLife.java
+++ GameOfLife.java
@@ -6,10 +6,13 @@ public class GameOfLife {
   
     //Current input reqs:
     //Line 1: n, where n is the number of lines of the n*n life array - n > 2
-    //Line 2: The 1st line of the life array (0 0 0 1 0 0 1 0)
+    //Line 2: The number of states you want simulated
+    //Line 3: The 1st line of the life array (0 0 0 1 0 0 1 0)
     Scanner input = new Scanner(System.in);
     String in = input.nextLine();
     int n = Integer.parseInt(in);
+    in = input.nextLine();
+    int states = Integer.parseInt(in);
     
     //Current state
     int[][] currentBoard = new int[n][n];
@@ -22,226 +25,231 @@ public class GameOfLife {
     }
     
     //Next state
-    int[][] nextBoard = new int[n][n];
-    for(int i = 0; i < n; i++){
-      for(int x = 0; x < n; x++){
-        //Finds neighbors of each cell. If there are 1 or >=4, it dies. If 2 it remains the same. If three it is alive
+    for(int w = 0; w < states; w++){
+      int[][] nextBoard = new int[n][n];
+      for(int i = 0; i < n; i++){
+        for(int x = 0; x < n; x++){
+          //Finds neighbors of each cell. If there are 1 or >=4, it dies. If 2 it remains the same. If three it is alive
+          
+          int alive = 0;
+          //The infinite wraparound grid method for extra funz - HARD CODED, COULD BE BAD BAD VERY BAD
+          //Calculate alive in the if statement block, calculate next state outside
+          if(i == 0 && x == 0){
+            //Top left corner
+            if(currentBoard[n - 1][n - 1] == 1){
+            alive++; 
+            }
+            if(currentBoard[n - 1][0] == 1){
+             alive++; 
+            }
+            if(currentBoard[n - 1][1] == 1){
+             alive++; 
+            }
+            for(int v = 0; v < 2; v++){
+              if(currentBoard[v][n - 1] == 1){
+                alive++;
+              }
+              if(currentBoard[v][1] == 1){
+                alive++;
+              }
+            }
+            if(currentBoard[1][0] == 1){
+                alive++;
+            }
+          
+          } else if (i == 0 && x == n - 1){
+            //Top right corner
+            if(currentBoard[0][0] == 1){
+            alive++; 
+            }
+            if(currentBoard[1][n - 2] == 1){
+             alive++; 
+            }
+            if(currentBoard[n - 1][n - 1] == 1){
+             alive++; 
+            }
+            if(currentBoard[n - 1][n - 2] == 1){
+             alive++; 
+            }
+            if(currentBoard[n - 1][0] == 1){
+             alive++; 
+            }
+            if(currentBoard[1][0] == 1){
+             alive++; 
+            }
+            for(int v = 0; v < 2; v++){
+              if(currentBoard[v][4] == 1){
+                alive++;
+              }
+            }
+            if(currentBoard[1][0] == 1){
+              alive++;
+            }
+          } else if (i == 0){
+            //Top row
+            for(int v = x - 1; v < x + 2; v++){
+              if(currentBoard[n - 1][v] == 1){
+                alive++; 
+              }
+            }  
+            if(currentBoard[0][x - 1] == 1){
+             alive++; 
+            }
+            if(currentBoard[0][x + 1] == 1){
+             alive++; 
+            }
+            for(int v = x + 1; v < x + 2; v++){
+              if(currentBoard[1][v] == 1){
+                alive++; 
+              }
+            }
+          } else if (x == 0 && i == n - 1){
+            //Bottom left corner
+          
+            if(currentBoard[0][n - 1] == 1){
+              alive++; 
+            }
+            if(currentBoard[n - 1][n - 1] == 1){
+             alive++; 
+            }
+            if(currentBoard[n - 2][n - 1] == 1){
+             alive++; 
+            }
+            if(currentBoard[n - 1][1] == 1){
+             alive++; 
+            }
+            for(int v = 0; v < 2; v++){
+              if(currentBoard[0][v] == 1){
+                alive++;
+              }
+              if(currentBoard[n - 2][v] == 1){
+                alive++;
+              }
+            }
+            if(currentBoard[n - 2][n - 1] == 1){
+                alive++;
+            }
+          
+          } else if (x == 0){
+            //Left side
+            for(int v = i - 1; v < i + 2; v++){
+              if(currentBoard[v][n - 1] == 1){
+                alive++; 
+              }
+              if(currentBoard[v][x + 1] == 1){
+                alive++; 
+              }
+            }  
+            if(currentBoard[i - 1][x] == 1){
+             alive++; 
+            }
+            if(currentBoard[i + 1][x] == 1){
+             alive++; 
+            }
+          
+          } else if (i == n - 1 && x == n - 1){
+            //Bottom right corner
+            
+            if(currentBoard[n - 2][0] == 1){
+                alive++;
+            }
+            if(currentBoard[0][0] == 1){
+                alive++;
+            }
+            for(int v = n - 1; v > n - 3; v--){
+              if(currentBoard[0][v] == 1){
+                  alive++;
+              }
+              if(currentBoard[n - 1][v] == 1){
+                  alive++;
+              }
+              if(currentBoard[n - 2][v] == 1){
+                  alive++;
+              }
+            }
+            
+          } else if (x == n - 1){
+            //Right side
+            for(int v = i - 1; v < i + 2; v++){
+              if(currentBoard[v][0] == 1){
+                alive++; 
+              }
+              if(currentBoard[v][x - 1] == 1){
+                alive++; 
+              }
+            }  
+            if(currentBoard[i - 1][x] == 1){
+             alive++; 
+            }
+            if(currentBoard[i + 1][x] == 1){
+             alive++; 
+            }
+          
+          } else if (i == n - 1){
+            //Bottom row
+            for(int v = x - 1; v < x + 2; v++){
+              if(currentBoard[0][v] == 1){
+                alive++; 
+              }
+            }  
+            if(currentBoard[i][x - 1] == 1){
+             alive++; 
+            }
+            if(currentBoard[i][x + 1] == 1){
+             alive++; 
+            }
+            for(int v = x - 1; v < x + 2; v++){
+              if(currentBoard[i - 1][v] == 1){
+                alive++; 
+              }
+            }
+          } else {
+            //Center
+            for(int v = x - 1; v < x + 2; v++){
+              if(currentBoard[i + 1][v] == 1){
+                alive++; 
+              }
+              if(currentBoard[i - 1][v] == 1){
+                alive++; 
+              }
+            }  
+            if(currentBoard[i][x - 1] == 1){
+             alive++; 
+            }
+            if(currentBoard[i][x + 1] == 1){
+             alive++; 
+            } 
+          }
         
-        int alive = 0;
-        //The infinite wraparound grid method for extra funz - HARD CODED, COULD BE BAD BAD VERY BAD
-        //Calculate alive in the if statement block, calculate next state outside
-        if(i == 0 && x == 0){
-          //Top left corner
-          if(currentBoard[n - 1][n - 1] == 1){
-           alive++; 
+          //Rules
+          if(alive < 2 || alive > 3){
+            nextBoard[i][x] = 0;
+          } else if (alive == 3){
+            nextBoard[i][x] = 1;
+          } else {
+            nextBoard[i][x] = currentBoard[i][x];
           }
-          if(currentBoard[n - 1][0] == 1){
-           alive++; 
-          }
-          if(currentBoard[n - 1][1] == 1){
-           alive++; 
-          }
-          for(int v = 0; v < 2; v++){
-            if(currentBoard[v][n - 1] == 1){
-              alive++;
-            }
-            if(currentBoard[v][1] == 1){
-              alive++;
-            }
-          }
-          if(currentBoard[1][0] == 1){
-              alive++;
-          }
-          
-        } else if (i == 0 && x == n - 1){
-          //Top right corner
-          if(currentBoard[0][0] == 1){
-           alive++; 
-          }
-          if(currentBoard[1][5] == 1){
-           alive++; 
-          }
-          if(currentBoard[n - 1][n - 1] == 1){
-           alive++; 
-          }
-          if(currentBoard[n - 1][n - 2] == 1){
-           alive++; 
-          }
-          if(currentBoard[n - 1][0] == 1){
-           alive++; 
-          }
-          if(currentBoard[1][0] == 1){
-           alive++; 
-          }
-          for(int v = 0; v < 2; v++){
-            if(currentBoard[v][4] == 1){
-              alive++;
-            }
-          }
-          if(currentBoard[1][0] == 1){
-              alive++;
-          }
-        } else if (i == 0){
-          //Top row
-          for(int v = x - 1; v < n + 2; v++){
-            if(currentBoard[n - 1][v] == 1){
-              alive++; 
-            }
-          }  
-          if(currentBoard[0][x - 1] == 1){
-           alive++; 
-          }
-          if(currentBoard[0][x + 1] == 1){
-           alive++; 
-          }
-          for(int v = x + 1; v < x + 2; v++){
-            if(currentBoard[1][v] == 1){
-              alive++; 
-            }
-          }
-        } else if (x == 0 && i == n - 1){
-          //Bottom left corner
-          
-          if(currentBoard[0][n - 1] == 1){
-           alive++; 
-          }
-          if(currentBoard[n - 1][n - 1] == 1){
-           alive++; 
-          }
-          if(currentBoard[n - 2][n - 1] == 1){
-           alive++; 
-          }
-          if(currentBoard[n - 1][1] == 1){
-           alive++; 
-          }
-          for(int v = 0; v < 2; v++){
-            if(currentBoard[0][v] == 1){
-              alive++;
-            }
-            if(currentBoard[n - 2][v] == 1){
-              alive++;
-            }
-          }
-          if(currentBoard[n - 2][n - 1] == 1){
-              alive++;
-          }
-          
-        } else if (x == 0){
-          //Left side
-          for(int v = i - 1; v < i + 2; v++){
-            if(currentBoard[v][n - 1] == 1){
-              alive++; 
-            }
-            if(currentBoard[v][x + 1] == 1){
-              alive++; 
-            }
-          }  
-          if(currentBoard[i - 1][x] == 1){
-           alive++; 
-          }
-          if(currentBoard[i + 1][x] == 1){
-           alive++; 
-          }
-          
-        } else if (i == n - 1 && x == n - 1){
-          //Bottom right corner
-          
-          if(currentBoard[n - 2][0] == 1){
-              alive++;
-          }
-          if(currentBoard[0][0] == 1){
-              alive++;
-          }
-          for(int v = n - 1; v > n - 3; v--){
-            if(currentBoard[0][v] == 1){
-                alive++;
-            }
-            if(currentBoard[n - 1][v] == 1){
-                alive++;
-            }
-            if(currentBoard[n - 2][v] == 1){
-                alive++;
-            }
-          }
-          
-        } else if (x == n - 1){
-          //Right side
-          for(int v = i - 1; v < i + 2; v++){
-            if(currentBoard[v][0] == 1){
-              alive++; 
-            }
-            if(currentBoard[v][x - 1] == 1){
-              alive++; 
-            }
-          }  
-          if(currentBoard[i - 1][x] == 1){
-           alive++; 
-          }
-          if(currentBoard[i + 1][x] == 1){
-           alive++; 
-          }
-          
-        } else if (i == n - 1){
-          //Bottom row
-          for(int v = x - 1; v < x + 2; v++){
-            if(currentBoard[0][v] == 1){
-              alive++; 
-            }
-          }  
-          if(currentBoard[i][x - 1] == 1){
-           alive++; 
-          }
-          if(currentBoard[i][x + 1] == 1){
-           alive++; 
-          }
-          for(int v = x - 1; v < x + 2; v++){
-            if(currentBoard[i - 1][v] == 1){
-              alive++; 
-            }
-          }
-        } else {
-          //Center
-          for(int v = x - 1; v < x + 2; v++){
-            if(currentBoard[i + 1][v] == 1){
-              alive++; 
-            }
-            if(currentBoard[i - 1][v] == 1){
-              alive++; 
-            }
-          }  
-          if(currentBoard[i][x - 1] == 1){
-           alive++; 
-          }
-          if(currentBoard[i][x + 1] == 1){
-           alive++; 
-          }
+        
         }
-        
-        //Rules
-        if(alive < 2 || alive > 3){
-          nextBoard[i][x] = 0;
-        } else if (alive == 3){
-          nextBoard[i][x] = 1;
-        } else {
-          nextBoard[i][x] = currentBoard[i][x];
-        }
-        
       }
+    
+      //Format output all nice and stuff
+      String border = "";
+      for(int i = 0; i < n; i++){
+        for(int x = 0; x < n; x++){
+          System.out.print(nextBoard[i][x] + " ");
+        }
+        System.out.println();
+        border += "--";
+      }
+    
+      System.out.println(border);
+      
+      currentBoard = nextBoard;
+      
     }
     
-    //Format output all nice and stuff
-    String border = "";
-    for(int i = 0; i < n; i++){
-      for(int x = 0; x < n; x++){
-        System.out.print(nextBoard[i][x] + " ");
-      }
-      System.out.println();
-      border += "-";
-    }
     
-    System.out.println(border);
-    
-    //TODO Set currentBoard equal to nextBoard and clear next board, then loop around
     
   }
 
