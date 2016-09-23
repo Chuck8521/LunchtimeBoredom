65642879a06579d5b9a6e023efc3e46453399d21
Chuck8521 <chuck8521@icloud.com>
2016-09-23 11:52:34 -0400

    Backup

--- Associativity.java
+++ Associativity.java
@@ -18,12 +18,26 @@ public class Associativity {
         String temp = inputString.nextLine();
         String[] letters = temp.split(" ");
         for(int y = 0; y < letters.length; y++){
-          nums[x][y] = Integer.parseInt(letters[y]));
+          int ascii = (int) letters[y];
+          nums[x][y] = asci - 97;
         }
       }
       
-      //TODO
-      
+      //TODO Test all letters and operations in the form (a @ a) @ a = a @ (a @ a)
+      int firstTotal = 0;
+      for(int v = 0; v < size; v++){
+        for(int y = 0; y < size; y++){
+          for(int x = 0; x < size; x++){
+            int test = nums[y][x];
+            int sum = nums[test][v];
+            int otherWay = nums[x][v];
+            int otherSum = nums[y][otherWay];
+            if(sum != otherSum){
+              System.out.println("not associative: " + )
+            }
+          }
+        }
+      }
     
     }
     
