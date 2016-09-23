2df85c426551f07109888c7547af2a89ddf1b356
Chuck8521 <chuck8521@icloud.com>
2016-09-23 12:02:28 -0400

    Create MeanMedianMode.java

---
+++ MeanMedianMode.java
@@ -0,0 +1,22 @@
+//2005 - #3
+
+import java.util.*;
+
+public class MeanMedianMode {
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    Scanner inputString = new Scanner(System.in);
+    int times = input.nextInt();
+
+    for(int i = 0; i < times; i++){
+      
+      
+      int size = input.nextInt();
+      
+    }
+    
+  }
+  
+}
