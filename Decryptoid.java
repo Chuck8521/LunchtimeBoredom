e79e8b2e6e1e4427dc9b4923fa275dacd295fe53
Chuck8521 <chuck8521@icloud.com>
2016-09-26 11:40:49 -0400

    Update Decryptoid.java

--- Decryptoid.java
+++ Decryptoid.java
@@ -8,12 +8,20 @@ public class Decryptoid {
 
     Scanner input = new Scanner(System.in);
     String in = input.nextLine();
+    String output = "";
     
     for(int i = 0; i < in.length(); i++){
       
+      char test = in.charAt(i);
+      if(isLetter(test)){
+        test + 2;
+      }
       
+      output += char;
       
     }
+    
+    System.out.println(output);
     
   }
   
