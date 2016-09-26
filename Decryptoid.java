3d8638dc08100a8b3ff2d8d59fb3083da4d71de5
Chuck8521 <chuck8521@icloud.com>
2016-09-26 07:59:47 -0400

    Create Decryptoid.java

---
+++ Decryptoid.java
@@ -0,0 +1,20 @@
+//Random riddle thing
+
+import java.util.*;
+
+public class Decryptoid {
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    String in = input.nextLine();
+    
+    for(int i = 0; i < in.length(); i++){
+      
+      
+      
+    }
+    
+  }
+  
+}
