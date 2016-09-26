f85a1d3b2856741c5c80023347dae154b3d24e1d
Chuck8521 <chuck8521@icloud.com>
2016-09-26 12:05:57 -0400

    Create CharRarityAnalyzer.java

---
+++ CharRarityAnalyzer.java
@@ -0,0 +1,23 @@
+//Random riddle thing
+
+import java.util.*;
+
+public class CharRarityAnalyzer {
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    String in = input.nextLine();
+    String output = "";
+    
+    for(int i = 0; i < in.length(); i++){
+        
+      char test = in.charAt(i);
+      
+    }
+    
+    System.out.println(output);
+    
+  }
+  
+}
