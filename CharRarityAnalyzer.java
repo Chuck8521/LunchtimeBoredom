99357134658e8ce3f796fbb774718caef4750039
Chuck8521 <chuck8521@icloud.com>
2016-09-27 11:53:49 -0400

    Ready to Test

--- CharRarityAnalyzer.java
+++ CharRarityAnalyzer.java
@@ -10,10 +10,25 @@ public class CharRarityAnalyzer {
     String in = input.nextLine();
     String output = "";
     
+    ArrayList<Character> letters = new ArrayList<Character>();
+    ArrayList<Integer> frequency = new ArrayList<Integer>();
+    
     for(int i = 0; i < in.length(); i++){
         
       char test = in.charAt(i);
+      if (!letters.contains(test)){
+        letters.add(test);
+        frequency.add(1);
+      } else {
+        frequency.set(letters.indexOf(test), frequency.get(letters.indexOf(test)) + 1);
+      }
       
+    }
+    
+    for(int i = 0; i < frequency.size(); i++){
+      if(frequency.get(i) == 1){
+        output += letters.get(i); 
+      }
     }
     
     System.out.println(output);
