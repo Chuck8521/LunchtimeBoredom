3e9cf53820bf3050b58599d07a300addc83a66bf
Chuck8521 <chuck8521@icloud.com>
2016-10-03 08:03:11 -0400

    Update SearchAlgorithms.java
    Just built a basic node class so I can keep states and info about them stored

--- SearchAlgorithms.java
+++ SearchAlgorithms.java
@@ -1,14 +1,29 @@
 import java.util.*;
 
+class Node(){
+  
+  List<Object> path = new ArrayList<Object>();
+  Object state;
+  int cost;
+  
+  public Node(List<Object> pathTo, Object currentState, int costTo){
+    this.path = pathTo;
+    this.state = currentState;
+    this.cost = costTo;
+  }
+  
+}
+
 class SearchAlgorithms {
 
   public static void main(String[] args){
     //Add code to select method of search
     //For now, just A* graph search
-    A*GraphSearch();
+    Object startState;//TODO
+    ArrayList<Object> solution = A*GraphSearch(startState);
   }
   
-  public static List<Object> A*GraphSearch(Object startState){
+  public static ArrayList<Object> A*GraphSearch(Object startState){
     HashSet<Object> closed = new HashSet<Object>();
     ArrayList<Object> fringe = new ArrayList<Object>();
     fringe.add(startState);
@@ -16,10 +31,11 @@ class SearchAlgorithms {
     while(true){
     
       if(fringe.size() == 0){
-        return null;//Valid?
+        return fringe;//essentially an empty list
       }
       
       //Remove the next node based on the heuristic
+      //TODO PriorityQueue?
       //if goal test is satisfied, return solution
       //if current state not in closed set then
         //add the node to closed
