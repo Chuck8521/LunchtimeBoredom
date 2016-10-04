7eb6ac57423f731392bdbcc1e562ac17b5783b33
Chuck8521 <chuck8521@icloud.com>
2016-10-03 21:02:38 -0400

    Working!
    Not optimized and pretty sloppy, but it works! A good start at least!

--- SearchAlgorithms.java
+++ SearchAlgorithms.java
@@ -26,8 +26,11 @@ class Point{
 }
 
 class NodeComparator implements Comparator<Node>{
+	
+  @Override
   public int compare(Node n1, Node n2) {
-      return n1.gCost - n2.gCost;
+	  //TODO AHHHHHHHHHHHHHHHHHHHHHHH HARD CODED
+      return n1.gCost(new Point(1,5)) - n2.gCost(new Point(1,5));
    }
 }
 
@@ -43,7 +46,7 @@ class Node {
     this.cost = costTo;//TODO cost problems on the comparator?
   }
   
-  @Override
+  //@Override
   public Node(Point currentState){
     //First node
     this.state = currentState;
@@ -59,6 +62,14 @@ class Node {
     return path; 
   }
   
+  public boolean isGoal(Point end){
+	  if((end.getX() == state.getX()) && (end.getY() == state.getY())){
+		  return true;
+	  } else {
+		  return false;
+	  }
+  }
+  
   public int gCost(Point end){
     return cost + state.getManhattenDist(end);
   }
@@ -68,32 +79,38 @@ class Node {
     //Check to see if Points above below left and right are open, or if there is a wall TODO Loop this
     if(!board[state.getX()][state.getY() + 1]){
       Point temp = new Point(state.getX(), state.getY() + 1);
-      path.add();
-      Node above = (path, temp, cost + 1);
+      ArrayList<Point> tempList = new ArrayList<Point>(path);
+      tempList.add(temp);
+      Node above = new Node(tempList, temp, cost + 1);
       moves.add(above);
-      path.remove(path.size() - 1);//Takes last element(what we just added) out
+      //path.remove(path.size() - 1);//Takes last element(what we just added) out
     }
     if(!board[state.getX()][state.getY() - 1]){//WARNING - Works only when walls border everything on all sides
       Point temp = new Point(state.getX(), state.getY() - 1);
-      path.add();
-      Node below = (path, temp, cost + 1);
+      ArrayList<Point> tempList = new ArrayList<Point>(path);
+      tempList.add(temp);
+      Node below = new Node(tempList, temp, cost + 1);
       moves.add(below);
-      path.remove(path.size() - 1);//Takes last element(what we just added) out
+      //path.remove(path.size() - 1);//Takes last element(what we just added) out
     }
     if(!board[state.getX() - 1][state.getY()]){
       Point temp = new Point(state.getX() - 1, state.getY());
-      path.add();
-      Node left = (path, temp, cost + 1);
+      ArrayList<Point> tempList = new ArrayList<Point>(path);
+      tempList.add(temp);
+      Node left = new Node(tempList, temp, cost + 1);
       moves.add(left);
-      path.remove(path.size() - 1);//Takes last element(what we just added) out
+      //path.remove(path.size() - 1);//Takes last element(what we just added) out
     }
     if(!board[state.getX() + 1][state.getY()]){
       Point temp = new Point(state.getX() + 1, state.getY());
-      path.add();
-      Node right = (path, temp, cost + 1);
+      ArrayList<Point> tempList = new ArrayList<Point>(path);
+      tempList.add(temp);
+      Node right = new Node(tempList, temp, cost + 1);
       moves.add(right);
-      path.remove(path.size() - 1);//Takes last element(what we just added) out
+      //path.remove(path.size() - 1);//Takes last element(what we just added) out
     }
+    
+    return moves;
   }
   
 }
@@ -107,18 +124,22 @@ class SearchAlgorithms {
     int lines = input.nextInt();     TODO INPUT STUFFS, FOR NOW JUST HARD CODE - TRUE = WALL*/
     boolean[][] board = {{true, true, true, true, true, true, true}, {true, false, false, true, false, false, true}, {true, true, false, true, false, false, true}, {true, false, false, true, false, false, true}, {true, false, false, true, false, true, true}, {true, false, false, false, false, false, true}, {true, true, true, true, true, true, true}};
     Point startState = new Point(5,1);
-    Point endState = new Point(0,5);
-    ArrayList<Point> solution = A*GraphSearch(startState, endState, board);
+    Point endState = new Point(1,5);
+    System.out.println("begin");
+    ArrayList<Point> solution = AStarGraphSearch(startState, endState, board);
     if(solution == null){
       System.out.println("failure"); 
     } else {
-      System.out.println("success"); 
+      System.out.println("success");
+      for(Point p : solution){
+         System.out.println("(" + p.getX() + "," + p.getY() + ")");
+      } 
     }
   }
   
   
   
-  public static ArrayList<Object> A*GraphSearch(Point startState, Point end, boolean[][] board){
+  public static ArrayList<Point> AStarGraphSearch(Point startState, Point end, boolean[][] board){
     HashSet<Point> closed = new HashSet<Point>();
     Comparator<Node> comparator = new NodeComparator();
     PriorityQueue<Node> fringe = new PriorityQueue<Node>(1, comparator);
@@ -126,7 +147,7 @@ class SearchAlgorithms {
     fringe.add(start);
     
     while(true){
-    
+    	System.out.println("continue");
       if(fringe.size() == 0){
         return null;//essentially an empty list? BAD PRACTICE TODO
       }
@@ -134,7 +155,7 @@ class SearchAlgorithms {
       //Remove the next node based on the heuristic
       Node test = fringe.poll();
       //if goal test is satisfied, return solution
-      if(test.gCost(end) == 0){
+      if(test.isGoal(end)){
         //Solution found!
         //Return path to main method
         return test.getPath();
@@ -144,7 +165,7 @@ class SearchAlgorithms {
         //add the node to closed
         closed.add(test.getCurrentState());
         //foreach child node of the current state
-        ArrayList<Node> children = allPossibleMoves(board);
+        ArrayList<Node> children = test.allPossibleMoves(board);
         for(Node n : children){
           //insert the node into the fringe
           fringe.add(n);
