1a2a0fe22629812d4412a27686bdbeddece3a858
Chuck8521 <chuck8521@icloud.com>
2016-10-11 12:14:53 -0400

    Create HillsAndPlateaus.java

---
+++ HillsAndPlateaus.java
@@ -0,0 +1,26 @@
+//2004 - #4
+
+import java.util.*;
+
+public class HillsAndPlateaus {
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    int times = input.nextInt();
+
+    for(int i = 0; i < times; i++){
+    
+      int numElements = input.nextInt();
+      ArrayList<Integer> sequence = new ArrayList<Integer>();
+      for(int x = 0; x < numElements; x++){
+        sequence.add(input.nextInt());
+      }
+      
+      //Find ascents, descents, plateaus
+    
+    }
+    
+  }
+  
+}
