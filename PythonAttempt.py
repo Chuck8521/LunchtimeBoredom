11aabced52233d1cf9dae6c112816fcd2b8e1b8a
Chuck8521 <chuck8521@icloud.com>
2016-10-31 08:00:55 -0400

    Create PythonAttempt.py

---
+++ PythonAttempt.py
@@ -0,0 +1,19 @@
+#2006 - 5
+#This is only a test.
+
+times = raw_input()
+while times > 0:
+  sizeOfTable = raw_input()
+  operationTable = []
+  i = 0
+  while i < sizeOfTable:
+    operationTable.append(raw_input())
+    i = i + 1;
+  numProblems = raw_input()
+  i = 0
+  while i < numProblems:
+    problem = raw_input()
+    
+    i = i + 1
+  
+  times = times - 1;
