166979196c84775658ef076dbf24f4cde05a96eb
Chuck8521 <chuck8521@icloud.com>
2017-02-07 13:34:09 -0500

    Working

--- 2015-7.py
+++ 2015-7.py
@@ -7,47 +7,36 @@ i = 0
 while i < times:
   
   line = raw_input()
-  print('0.0' + line + ' = '),
+  print('0.' + line + ' ='),
   
   parts = map(str, line.split())
   
   temp = parts[0]
   temp = '0.' + temp
-  #temp = float(temp)
+
   repDigit = int(parts[1])
   
   #All digits plus one set repeating - all digits
   
   bigNumber = float(temp)
   spliceSpot = int(len(temp) - repDigit)
-  print(spliceSpot)
   smallNumber = float(temp[0:spliceSpot])
   
   smallNumber *= float(math.pow(10,(len(parts[0]) - repDigit)))
   bigNumber *= float(math.pow(10,len(parts[0])))
   
-  #print(smallNumber)
-  #print(bigNumber)
   
   numer = bigNumber - smallNumber
-  print(numer)
-  denom = ''
+  denom = float(math.pow(10,len(parts[0])))
+  denom -= float(math.pow(10,(len(parts[0]) - repDigit)))
+    
+  GCD = gcd(numer,denom)
+  numer /= GCD
+  denom /= GCD
   
-  j = 0
-  while j < repDigit:
-    denom += '9'
-    j += 1
-  
-  while len(denom) <= len(str(numer)) - 1:
-    denom += '0'
-  
+  numer = int(numer)
   denom = int(denom)
   
-  numer /= gcd(numer,denom)
-  denom /= gcd(numer,denom)
-  
-  print(numer),
-  print('/'),
-  print(denom)
+  print(str(numer) + '/' + str(denom))
   
   i += 1
