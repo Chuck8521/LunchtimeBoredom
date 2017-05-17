d7953ba47da95e184f5835d70456be7ab959224d
Chuck8521 <chuck8521@icloud.com>
2017-05-17 16:37:57 -0400

    Update woc32p3.py

--- woc32p3.py
+++ woc32p3.py
@@ -5,44 +5,26 @@ import sys
 
 def circularWalk(n, s, t, r_0, g, seed, p):
     # Complete this function
-    dist = []
-    boolMatrix = [[] for w in range(n)]
-    for w in range(n):
-        for e in range(n):
-            boolMatrix[w].append(0)
-    for q in range(n):
-        if q == 0:
-            dist.append(r_0)
-            i = 1
-            while i <= r_0:
-                boolMatrix[q][q+i] = 1
-                boolMatrix[q][q-i] = 1
-                i += 1
-        else:
-            dist.append((dist[q - 1] * g + seed) % p)
-            i = 1
-            while i <= (dist[q - 1] * g + seed) % p:
-                if q+i < n:
-                    boolMatrix[q][q+i] = 1
-                else:
-                    boolMatrix[q][(q+i) % n] = 1
-                boolMatrix[q][q-i] = 1
-                i += 1
-                
-    #boolMatrix is now right CONFIRMED. See what's going on
+    
+    if s == t:
+        return 0
+    
     visited = []
     currentPoints = [s]
     steps = 0
-    #When t is in visited
-    while t not in visited:
+    #When t is in visited, stop
+    while t not in currentPoints:
+        
+        if len(currentPoints) == 0:
+            return -1
+        
         newCurr = []
         for vertex in currentPoints:
-            line = boolMatrix[vertex]
-            for index, num in enumerate(line):
-                if num == 1 and index not in newCurr and index not in visited and index not in currentPoints:
-                    newCurr.append(index)
+            #TODO
+            if index not in newCurr and index not in visited and index not in currentPoints:
+                newCurr.append(index)
                     
-        #visited += newCurr
+        #visited += current
         for vertex in currentPoints:
             if vertex not in visited:
                 visited.append(vertex)
@@ -54,8 +36,9 @@ def circularWalk(n, s, t, r_0, g, seed, p):
         for v in currentPoints:
             if v in visited:
                 currentPoints.remove(v)
+                
     
-    return steps - 1 #Because of how stuff is added to visited
+    return steps
             
 
 n, s, t = input().strip().split(' ')
